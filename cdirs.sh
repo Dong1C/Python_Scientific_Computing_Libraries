a2aa94bde956f23c0a8d0491e435cfc3db8b43b4
Dong1C <Dong1C@noreply.gitcode.com>
2024-07-03 09:55:10 +0700

    first create all files

---
+++ cdirs.sh
@@ -0,0 +1,6 @@
+for dir in 01_numpy 02_pandas 03_scipy 04_matplotlib 05_seaborn 06_sympy 07_statsmodels 08_tensorflow 09_keras 10_pytorch 11_sklearn 12_networkx 13_h5py
+do
+    mkdir -p $dir
+    touch $dir/README.md
+done
+
